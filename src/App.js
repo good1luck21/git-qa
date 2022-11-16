b7e30d616ebb26e927fba7381d50ee67e223f011
good1luck21 <good1luck21@gmail.com>
2022-11-16 12:46:14 +0900

    github pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+// import styled css
+import styled from "styled-components";
+
+// create a header logo and nav bar will be separate components
+const Header = styled.header`
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`;
 
 function App() {
+  const [count, setCount] = useState(0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header>
+        <p>{count}</p>
+        <button onClick={() => setCount(count + 1)}>Increment</button>
+      </Header>
     </div>
   );
 }
